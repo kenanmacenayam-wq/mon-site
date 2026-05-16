78ca52b406f44dc9e894dd75932e13673b995172
kenanmacenayam-wq <kenan.macenayam@gmail.com>
2026-05-16 18:25:09 +0200

    message

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const bouton = document.getElementById("bouton");
 
 const texte = document.getElementById("texte");
 
+etat = 0
+
 bouton.addEventListener("click", () => {
-    texte.innerText = "Le JavaScript fonctionne !";
+    if (etat==0){
+        texte.innerText = "Le JavaScript fonctionne !";
+        etat = 1;
+    }else{
+        texte.innerText = "May be !";
+        etat = 0;
+    }
 });
